15ef7ebfaa3223ab8aa1dff14345139f52b7934c
Alexander.Li <alexander.li@CNSHHQ-0291.ef.com>
2015-07-08 18:54:38 +0800

    add email functionality in bulletin module

--- app/view/bulletin/editbulletin.js
+++ app/view/bulletin/editbulletin.js
@@ -3,7 +3,7 @@ Ext.define('FamilyDecoration.view.bulletin.EditBulletin', {
     width: 500,
     height: 400,
     modal: true,
-    layout: 'fit',
+    layout: 'vbox',
 
     bulletin: null,
     
@@ -16,7 +16,17 @@ Ext.define('FamilyDecoration.view.bulletin.EditBulletin', {
     		xtype: 'textareafield',
             emptyText: '请输入公告内容',
             hideLabel: true,
+            width: '100%',
+            flex: 9,
             value: me.bulletin ? unescape(me.bulletin.get('content')) : ''
+    	}, {
+    		xtype: 'checkbox',
+    		boxLabel: '邮件通知',
+    		hideLabel: true,
+    		width: '100%',
+            flex: 1,
+    		id: 'checkbox-announcementViaEmail',
+    		name: 'checkbox-announcementViaEmail'
     	}];
 
     	me.buttons = [{
@@ -25,7 +35,8 @@ Ext.define('FamilyDecoration.view.bulletin.EditBulletin', {
 	            var textarea = me.down('textareafield'),
 	            	p = {
 	                    content: escape(textarea.getValue())
-	                };
+	                },
+	                chk = Ext.getCmp('checkbox-announcementViaEmail');
 	            me.bulletin && Ext.apply(p, {
 	            	id: me.bulletin.getId()
 	            })
@@ -44,6 +55,27 @@ Ext.define('FamilyDecoration.view.bulletin.EditBulletin', {
 	                    }
 	                }
 	            });
+
+	            if (chk.getValue()) {
+	            	Ext.Ajax.request({
+						url: './libs/user.php?action=view',
+						method: 'GET',
+						callback: function (opts, success, res){
+							if (success) {
+								var userArr = Ext.decode(res.responseText);
+								var content = User.getRealName() + '发布了公告，公告内容为：\n' + textarea.getValue(),
+									subject = '公告通知';
+								for (i = 0; i < userArr.length; i++) {
+									setTimeout((function (index){
+										return function (){
+											sendMail(userArr[index].name, userArr[index].mail, subject, content);
+										}
+									})(i), 1000 * (i + 1));
+								}
+							}
+						}
+					});
+	            }
 	        }
 	    }, {
 	        text: '取消',
