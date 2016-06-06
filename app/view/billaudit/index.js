333f5aea184f7f8e23765ba38f0fca691ad52151
alex123bob@126.com <alex123bob@126.com>
2016-06-06 11:41:10 +0800

    partially finish bill auditing functions. ready to add logic operations for buttons

--- app/view/billaudit/index.js
+++ app/view/billaudit/index.js
@@ -5,7 +5,8 @@ Ext.define('FamilyDecoration.view.billaudit.Index', {
         'FamilyDecoration.store.WorkCategory',
         'FamilyDecoration.view.manuallycheckbill.BillTable',
         'FamilyDecoration.store.StatementBill',
-        'Ext.ux.form.SearchField'
+        'Ext.ux.form.SearchField',
+        'FamilyDecoration.view.billaudit.BillList'
     ],
     layout: 'hbox',
 
@@ -16,114 +17,68 @@ Ext.define('FamilyDecoration.view.billaudit.Index', {
         me.getRes = function () {
             var billList = Ext.getCmp('gridpanel-billListForAudit'),
                 bill = billList.getSelectionModel().getSelection()[0],
+                passedBillList = Ext.getCmp('gridpanel-passedBillListForAudit'),
+                passedBill = passedBillList.getSelectionModel().getSelection()[0],
                 billDetailCt = Ext.getCmp('billtable-billDetailForAudit');
                 
             return {
                 billList: billList,
                 bill: bill,
+                passedBillList: passedBillList,
+                passedBill: passedBill,
                 billDetailCt: billDetailCt
             }
         };
 
-        var billSt = Ext.create('FamilyDecoration.store.StatementBill', {
-            autoLoad: false
-        });
-        
-        billSt.load({
-            params: {
-                action: 'StatementBill.get',
-                orderby: 'createTime DESC',
-                status: 'rdyck'
-            }
-        });
-
         me.items = [
             {
-                xtype: 'gridpanel',
-                id: 'gridpanel-billListForAudit',
-                name: 'gridpanel-billListForAudit',
-                title: '账单列表',
+                xtype: 'container',
                 flex: 1,
+                layout: 'vbox',
                 height: '100%',
-                style: {
-                    borderRightStyle: 'solid',
-                    borderRightWidth: '1px'
-                },
-                hideHeaders: true,
-                dockedItems: [{
-					dock: 'top',
-					xtype: 'toolbar',
-					items: [{
-						xtype: 'searchfield',
-						flex: 1,
-						store: billSt,
-						paramName: 'billName'
-					}]
-				}],
-                autoScroll: true,
-                columns: {
-                    items: [
-                        {
-                            text: '单名',
-                            dataIndex: 'billName'
+                items: [
+                    {
+                        title: '待审核账单',
+                        style: {
+                            borderRightStyle: 'solid',
+                            borderRightWidth: '1px'
+                        },
+                        xtype: 'billaudit-billlist',
+                        id: 'gridpanel-billListForAudit',
+                        name: 'gridpanel-billListForAudit',
+                        flex: 2,
+                        width: '100%',
+                        selectionchangeEvent: function (selModel, sels, opts){
+                            var rec = sels[0],
+                                    resourceObj = me.getRes();
+                                resourceObj.billDetailCt.initBtn();
+                                resourceObj.billDetailCt.bill = rec;
+                                resourceObj.billDetailCt.refresh(rec);
+                                resourceObj.passedBillList.getSelectionModel().deselectAll();
                         }
-                        // {
-                        //     text: '项目',
-                        //     dataIndex: 'projectName'
-                        // },
-                        // {
-                        //     text: '工种',
-                        //     dataIndex: 'professionType',
-                        //     renderer: function (val, meta, rec) {
-                        //         return FamilyDecoration.store.WorkCategory.renderer(val);
-                        //     }
-                        // }
-                    ],
-                    defaults: {
-                        flex: 1,
-                        align: 'center'
-                    }
-                },
-                store: billSt,
-                listeners: {
-                    afterrender: function (grid, opts) {
-                        var view = grid.getView();
-                        var tip = Ext.create('Ext.tip.ToolTip', {
-                            // The overall target element.
-                            target: view.el,
-                            // Each grid row causes its own separate show and hide.
-                            delegate: view.cellSelector,
-                            // Moving within the row should not hide the tip.
-                            trackMouse: true,
-                            // Render immediately so that tip.body can be referenced prior to the first show.
-                            renderTo: Ext.getBody(),
-                            listeners: {
-                                // Change content dynamically depending on which element triggered the show.
-                                beforeshow: function updateTipBody(tip) {
-                                    var gridColumns = view.getGridColumns();
-                                    var column = gridColumns[tip.triggerElement.cellIndex];
-                                    var rec = view.getRecord(tip.triggerElement.parentNode);
-                                    // var val = rec.get(column.dataIndex);
-                                    val = '<strong>项目名称：</strong> ' + rec.get('projectName') + '<br />';
-                                    val += '<strong>工种：</strong> ' + FamilyDecoration.store.WorkCategory.renderer(rec.get('professionType')) + '<br />';
-                                    val += '<strong>总金额：</strong> ' + rec.get('totalFee') + '<br />';
-                                    val += '<strong>领款人：</strong> ' + rec.get('payee') + '<br />';
-                                    val += '<strong>申领金额：</strong> ' + rec.get('claimAmount') + '<br />';
-                                    val += '<strong>完成情况：</strong> ' + rec.get('projectProgress') + '<br />';
-                                    val += '<strong>预算总价：</strong> ' + rec.get('totalFee') + '<br />';
-                                    val += '<strong>创建时间：</strong> ' + rec.get('createTime') + '<br />';
-                                    tip.update(val);
-                                }
-                            }
-                        });
                     },
-                    selectionchange: function (selModel, sels, opts){
-                        var rec = sels[0],
-                            resourceObj = me.getRes();
-                        resourceObj.billDetailCt.bill = rec;
-                        resourceObj.billDetailCt.refresh(rec);
+                    {
+                        title: '已审核账单',
+                        style: {
+                            borderRightStyle: 'solid',
+                            borderRightWidth: '1px'
+                        },
+                        xtype: 'billaudit-billlist',
+                        id: 'gridpanel-passedBillListForAudit',
+                        name: 'gridpanel-passedBillListForAudit',
+                        flex: 1,
+                        width: '100%',
+                        isPassedBillList: true,
+                        selectionchangeEvent: function (selModel, sels, opts){
+                            var rec = sels[0],
+                                    resourceObj = me.getRes();
+                                resourceObj.billDetailCt.initBtn();
+                                resourceObj.billDetailCt.bill = rec;
+                                resourceObj.billDetailCt.refresh(rec);
+                                resourceObj.billList.getSelectionModel().deselectAll();
+                        }
                     }
-                }
+                ]
             },
             {
                 xtype: 'manuallycheckbill-billtable',
@@ -134,7 +89,54 @@ Ext.define('FamilyDecoration.view.billaudit.Index', {
                 header: true,
                 height: '100%',
                 isPreview: true,
-                isAudit: true
+                isAudit: true,
+                getButtons: function (){
+                    var panel = this;
+					return {
+						returnBill: panel.query('[name="returnBill"]')[0],
+                        auditPass: panel.query('[name="auditPass"]')[0],
+                        financialPayment: panel.query('[name="financialPayment"]')[0]
+					};
+                },
+                initBtn: function (){
+                    var resourceObj = me.getRes(),
+                        btns = this.getButtons();
+                    for (var btnKey in btns) {
+                        if (btns.hasOwnProperty(btnKey)) {
+                            var btnEl = btns[btnKey];
+                            btnEl.setDisabled(!resourceObj.bill);
+                        }
+                    }
+                },
+                tbar: [
+                    {
+                        text: '退回单据',
+                        disabled: true,
+                        name: 'returnBill',
+                        icon: 'resources/img/returnBill.png',
+                        handler: function (){
+                            
+                        }
+                    },
+                    {
+                        text: '审核通过',
+                        disabled: true,
+                        name: 'auditPass',
+                        icon: 'resources/img/auditPass.png',
+                        handler: function (){
+                            
+                        }
+                    },
+                    {
+                        text: '财务付款',
+                        disabled: true,
+                        name: 'financialPayment',
+                        icon: 'resources/img/pay.png',
+                        handler: function (){
+                            
+                        }
+                    }
+                ]
             }
         ];
 
