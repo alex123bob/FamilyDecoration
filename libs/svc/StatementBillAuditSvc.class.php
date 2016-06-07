ce7ce0a04b71fe8a6266585d9538ea0782a12402
diegozhu <diego@diegozhu.net>
2016-06-07 21:19:40 +0800

    add status chinese name

--- libs/svc/StatementBillAuditSvc.class.php
+++ libs/svc/StatementBillAuditSvc.class.php
@@ -5,5 +5,16 @@ class StatementBillAuditSvc extends BaseSvc
 		$q['@id'] = $this->getUUID();
 		return parent::add($q);
 	}
+	public function get($q){
+		$res = parent::get($q);
+		$billSvc = $this->getSvc('StatementBill');
+		foreach ($res['data'] as &$v) {
+			if(isset($v['orignalStatus']))
+				$v['orignalStatusName'] = StatementBillSvc::$statusMapping[$v['orignalStatus']];
+			if(isset($v['newStatus']))
+				$v['newStatusName'] = StatementBillSvc::$statusMapping[$v['newStatus']];
+		}
+		return $res;
+	}
 }
 ?>
