f917f4bd85edf218edf7007e7b24eea6ce0fda08
alex123bob@126.com <alex123bob@126.com>
2016-06-10 23:00:13 +0800

    add cny function in php side to transfer financial number to Chinese uppercase characters.

--- libs/common.php
+++ libs/common.php
@@ -181,7 +181,65 @@
 		}
 		return $res;
     }
-
+	/** 
+    *数字金额转换成中文大写金额的函数 
+    *String Int  $num  要转换的小写数字或小写字符串 
+    *return 大写字母 
+    *小数位为两位 
+    **/  
+    function cny($num){  
+        $c1 = "零壹贰叁肆伍陆柒捌玖";  
+        $c2 = "分角元拾佰仟万拾佰仟亿";  
+        $num = round($num, 2);  
+        $num = $num * 100;  
+        if (strlen($num) > 10) {  
+            return "数据太长，没有这么大的钱吧，检查下";  
+        }
+        $i = 0;  
+        $c = "";  
+        while (1) {  
+            if ($i == 0) {  
+                $n = substr($num, strlen($num)-1, 1);  
+            } else {  
+                $n = $num % 10;  
+            }   
+            $p1 = substr($c1, 3 * $n, 3);  
+            $p2 = substr($c2, 3 * $i, 3);  
+            if ($n != '0' || ($n == '0' && ($p2 == '亿' || $p2 == '万' || $p2 == '元'))) {  
+                $c = $p1 . $p2 . $c;  
+            } else {  
+                $c = $p1 . $c;  
+            }   
+            $i = $i + 1;  
+            $num = $num / 10;  
+            $num = (int)$num;  
+            if ($num == 0) {  
+                break;  
+            }   
+        }  
+        $j = 0;  
+        $slen = strlen($c);  
+        while ($j < $slen) {  
+            $m = substr($c, $j, 6);  
+            if ($m == '零元' || $m == '零万' || $m == '零亿' || $m == '零零') {  
+                $left = substr($c, 0, $j);  
+                $right = substr($c, $j + 3);  
+                $c = $left . $right;  
+                $j = $j-3;  
+                $slen = $slen-3;  
+            }   
+            $j = $j + 3;  
+        }   
+  
+        if (substr($c, strlen($c)-3, 3) == '零') {  
+            $c = substr($c, 0, strlen($c)-3);  
+        }  
+        if (empty($c)) {  
+            return "零元整";  
+        }else{  
+            return $c . "整";  
+        }  
+    }
     function notNullCheck($d,$f,$msg =""){
     	if(!isset($d[$f]) || $d[$f] == ""){
     		if($msg == "")
