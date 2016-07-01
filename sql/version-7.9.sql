cc85158a30a90af5e19ad9c4e9e5a5393c305322
alex123bob@126.com <alex123bob@126.com>
2016-07-01 23:16:28 +0800

    remove id in sql script coz it's auto_increment.

--- sql/version-7.9.sql
+++ sql/version-7.9.sql
@@ -1,3 +1,3 @@
-insert into `system` (`id`, `paramDesc`, `isDeleted`, `updateTime`, `paramName`, `createTime`, `paramValue`) 
-	values ( '12','财务单据状态更改通知短信内容模板:您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!', 'false', '0000-00-00 00:00:00', 'msg_notice_bill_status_change', '2016-06-26 14:09:54', '您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!');
+insert into `system` (`paramDesc`, `isDeleted`, `updateTime`, `paramName`, `createTime`, `paramValue`) 
+	values ('财务单据状态更改通知短信内容模板:您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!', 'false', '0000-00-00 00:00:00', 'msg_notice_bill_status_change', '2016-06-26 14:09:54', '您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!');
 update `system` set `paramValue`='version-7.9' where `id`='4';
