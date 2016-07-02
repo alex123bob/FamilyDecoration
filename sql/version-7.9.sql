8228f5414c788cd377b2111f19c55b4c1a9e6d2b
alex123bob@126.com <alex123bob@126.com>
2016-07-02 09:33:23 +0800

    change system param name

--- sql/version-7.9.sql
+++ sql/version-7.9.sql
@@ -1,3 +1,4 @@
 insert into `system` (`paramDesc`, `isDeleted`, `updateTime`, `paramName`, `createTime`, `paramValue`) 
 	values ('财务单据状态更改通知短信内容模板:您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!', 'false', '0000-00-00 00:00:00', 'msg_notice_bill_status_change', '2016-06-26 14:09:54', '您好,{申请人}的财务订单{单号},{项目}项目,总金额:{总金额}元,申领金额:{申领金额}元,已被{操作人}变更为{现状态}!');
+UPDATE `familydecoration`.`system` SET `paramName` = 'msg_notice_value_limit' WHERE `system`.`id` = 11;
 update `system` set `paramValue`='version-7.9' where `id`='4';
