4da2bb7dd021aa08aa693adf14309a549df3010d
alex123bob@126.com <alex123bob@126.com>
2016-07-02 10:48:55 +0800

    add isDecorated field

--- sql/version-8.0.sql
+++ sql/version-8.0.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `region` ADD `openingTime` TIMESTAMP NULL COMMENT '开盘时间' AFTER `nameRemark`;
+ALTER TABLE `potential_business` ADD `isDecorated` VARCHAR(5) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否装修' AFTER `phone`;
+
 update `system` set `paramValue`='version-8.0' where `id`='4';
