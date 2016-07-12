719340dc70f7905ad5d6a5241945a05c57a409ed
diegozhu <674417307@qq.com>
2016-07-12 17:27:28 +0800

    libs/svc/

---
+++ libs/svc/LogListSvc.class.php
@@ -0,0 +1,14 @@
+<?php
+class LogListSvc extends BaseSvc
+{
+	public function getMonths(){
+		return array(
+			array('year'=>'2016','month'=>'05'),
+			array('year'=>'2016','month'=>'06'),
+			array('year'=>'2016','month'=>'07')
+		);
+	}
+
+}
+
+?>
