ba933bfa49d010c18d07d04e3ab0b4fd98ef911c
diegozhu <674417307@qq.com>
2016-07-12 17:37:07 +0800

    update loglist

--- libs/svc/LogListSvc.class.php
+++ libs/svc/LogListSvc.class.php
@@ -2,13 +2,14 @@
 class LogListSvc extends BaseSvc
 {
 	public function getMonths(){
+		$now = new DateTime('now');
+		$month = $now->format('m');
+		$year = $now->format('Y');
 		return array(
-			array('year'=>'2016','month'=>'05'),
-			array('year'=>'2016','month'=>'06'),
-			array('year'=>'2016','month'=>'07')
+			array('year'=>$month - 2 < 1 ? $year - 1 : (int)$year,'month'=>$month - 2 < 1 ? 12 : $month - 2 ),
+			array('year'=>$month - 1 < 1 ? $year - 1 : (int)$year,'month'=>$month - 1 < 1 ? 12 : $month - 1 ),
+			array('year'=>(int)$year,'month'=>(int)$month)
 		);
 	}
-
 }
-
 ?>
