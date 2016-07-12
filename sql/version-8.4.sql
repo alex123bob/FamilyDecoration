e37108be02584fc4cb7ce40db6a7d8cbb4dd8350
diegozhu <674417307@qq.com>
2016-07-12 16:15:32 +0800

    add error log

---
+++ sql/version-8.4.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `error_log` (
+  `detail` text DEFAULT NULL,
+  `user` varchar(500) DEFAULT NULL,
+  `file` varchar(200) DEFAULT NULL,
+  `line` varchar(20) DEFAULT NULL,
+  `isDeleted` varchar(5) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+update `system` set `paramValue`='version-8.4' where `id`='4';
