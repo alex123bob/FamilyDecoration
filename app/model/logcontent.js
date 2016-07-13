90081d7791ceb74973c587fd2603a7ceed28eb23
alex123bob@126.com <alex123bob@126.com>
2016-07-14 00:52:43 +0800

    add two more fields into logcontent model for future summary's and comments' modification

--- app/model/logcontent.js
+++ app/model/logcontent.js
@@ -6,7 +6,9 @@ Ext.define('FamilyDecoration.model.LogContent', {
         {name: 'practicalAccomplishment', type: 'string', mapping: 'pa'},
         {name: 'difference', type: 'string', mapping: 'd'},
         {name: 'selfPlan', type: 'string', mapping: 's'},
+        {name: 'summarizedLogId', type: 'string', mapping: 'sid'},
         {name: 'summarizedLog', type: 'string', mapping: 'sl'},
+        {name: 'commentsId', type: 'string', mapping: 'cid'},
         {name: 'comments', type: 'string', mapping: 'c'},
         {name: 'day', type: 'string', mapping: 'dy'}
     ],
