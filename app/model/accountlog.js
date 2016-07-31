af8dceb66c405f9b8dd9ebfaaf7f563c4ddc40b8
alex123bob@126.com <alex123bob@126.com>
2016-08-01 00:05:44 +0800

    add operator in account management module

--- app/model/accountlog.js
+++ app/model/accountlog.js
@@ -8,6 +8,7 @@ Ext.define('FamilyDecoration.model.AccountLog', {
         {name: 'refId', type: 'string'}, // 关联单据，工资单(salary)或者贷款单(loan)或者其他(statement_bill)单据Id，或-1(修改余额操作)
         {name: 'refType', type: 'string'}, // 关联单据类型，sly:工资单(salary); loan:贷款单(loan);stb:其他(statement_bill)单据Id;edit:修改
         {name: 'operator', type: 'string'},
+        {name: 'operatorRealName', type: 'string'},
         'balance', // 操作后余额
         {name: 'isDeleted', type: 'string'},
         {name: 'createTime', type: 'string'},
