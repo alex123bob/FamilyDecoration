e6d2056f2aa1f15a8b11f39f720610aa32cd6a14
alex123bob@126.com <alex123bob@126.com>
2016-07-31 17:07:42 +0800

    initialize account panel

--- app/view/account/index.js
+++ app/view/account/index.js
@@ -3,14 +3,47 @@ Ext.define('FamilyDecoration.view.account.Index', {
     alias: 'widget.account-index',
     title: '账户管理',
     requires: [
-        
+        'FamilyDecoration.store.Account'
     ],
     layout: 'hbox',
+    defaults: {
+        xtype: 'gridpanel',
+        height: '100%'
+    },
 
     initComponent: function () {
         var me = this;
 
-        me.items = [];
+        me.items = [
+            {
+                title: '账户',
+                flex: 1,
+                columns: {
+                    defaults: {
+                        flex: 1,
+                        align: 'center'
+                    },
+                    items: [
+                        {
+                            text: '账户名称',
+                            dataIndex: 'name'
+                        },
+                        {
+                            text: '账户类型',
+                            dataIndex: 'accountType'
+                        }
+                    ]
+                },
+                store: Ext.create('FamilyDecoration.store.Account', {
+                    autoLoad: true
+                })
+            },
+            {
+                title: '近五日纪录',
+                flex: 4,
+                columns: []
+            }
+        ];
 
         this.callParent();
     }
