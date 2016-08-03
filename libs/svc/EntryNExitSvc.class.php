def2b65c1e3b5e35ae0e681f3c01c0a870be5b5f
diegozhu <zhuhaiyang55@gmail.com>
2016-08-03 21:39:23 +0800

    paging and filter by id

--- libs/svc/EntryNExitSvc.class.php
+++ libs/svc/EntryNExitSvc.class.php
@@ -54,8 +54,11 @@ class EntryNExitSvc{
 					b.status
 					from statement_bill b left join user u on u.name = b.payer
 					where b.billType = 'wlf' and b.isDeleted = 'false' and ( b.status = 'paid' or b.status = 'chk')";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -76,8 +79,11 @@ class EntryNExitSvc{
 					b.status
 					from statement_bill b left join user u on u.name = b.payer
 					where b.billType = 'tax' and b.isDeleted = 'false' and ( b.status = 'paid' or b.status = 'chk')";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -134,8 +140,11 @@ class EntryNExitSvc{
 					b.status,
 					payer as c11 from statement_bill b left join profession_type t on b.professionType = t.value
 					where b.isDeleted = 'false' and (b.billType = 'reg' or b.billType = 'ppd') and ( b.status = 'paid' or b.status = 'chk')";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -155,8 +164,11 @@ class EntryNExitSvc{
 					s.paidTime as c10,
 					u2.realName as c11
 					from salary s left join user u on u.name = s.payee left join user u2 on u2.name = s.payer where s.isDeleted = 'false' and status != 'arch'";
-		if(isset($q['c0'])){
-			$sql .= ' and s.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and s.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and u.realName like \'%'.$q['payee'].'%\'';
 		}
 		$res = $this->parseData($sql,$q);
 		$userSvc = BaseSvc::getSvc('User');
@@ -183,8 +195,11 @@ class EntryNExitSvc{
 					u.realName as c12 
 					from statement_bill b left join supplier s on b.supplierId = s.id 
 					left join user u on u.name = b.payer where b.billType = 'mtf' and b.isDeleted = 'false' and ( b.status = 'paid' or b.status = 'chk')";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -203,8 +218,11 @@ class EntryNExitSvc{
 					'TODO' as c9 
 					from statement_bill b left join user u on u.name = b.payer 
 					where b.billType = 'rbm' and b.isDeleted = 'false' and  ( b.status = 'paid' or b.status = 'chk')";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -227,8 +245,11 @@ class EntryNExitSvc{
 					left join user u2 on u2.name = l.dealer
 					left join loan l2 on l2.id = l.relevantId
  					where l.isDeleted = 'false' and l.type = '1' and l.status != 'arch'";
- 		if(isset($q['c0'])){
-			$sql .= ' and l.id = \''.$q['c0'].'\'';
+ 		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and l.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and u.realName like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -248,8 +269,11 @@ class EntryNExitSvc{
 					l.loanTime as c10 
 					from loan l left join user u on u.name = l.assignee left join user u2 on u2.name = l.dealer
  					where l.isDeleted = 'false' and l.type = '0' and l.status != 'arch'";
- 		if(isset($q['c0'])){
-			$sql .= ' and l.id = \''.$q['c0'].'\'';
+ 		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and l.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and u.realName like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -266,8 +290,11 @@ class EntryNExitSvc{
 					b.payer as c7
 					from statement_bill b left join user u on u.name = b.payer left join project p on p.projectId = b.projectId left join business bs on bs.id = p.businessId
 					where b.billType = 'dsdpst' and b.isDeleted = 'false' and  b.status = 'accepted'";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -277,18 +304,21 @@ class EntryNExitSvc{
 		$sql = "select b.id as c0,
 					b.projectName as c1,
 					b.reimbursementReason as c2,
-					b.payee as c2,
-					b.phoneNumber as c3,
-					b.totalFee as c4,
-					b.paidAmount as c5,
-					u.realName as c6,
-					b.paidTime as c7,
-					b.descpt as c8,
+					b.payee as c3,
+					b.phoneNumber as c4,
+					b.totalFee as c5,
+					b.paidAmount as c6,
+					u.realName as c7,
+					b.paidTime as c8,
+					b.descpt as c9,
 					b.status
 					from statement_bill b left join user u on u.name = b.payer
 					where b.billType = 'other' and b.isDeleted = 'false' and b.status = 'accepted'";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
@@ -306,9 +336,12 @@ class EntryNExitSvc{
 					b.paidTime as c9,
 					b.reimbursementReason as c8
 					from statement_bill b left join user u on u.name = b.payer left join project p on p.projectId = b.projectId left join business bs on bs.id = p.businessId
-					where b.billType = 'pjtf' and b.isDeleted = 'false' and b.status = 'accepted';";
-		if(isset($q['c0'])){
-			$sql .= ' and b.id = \''.$q['c0'].'\'';
+					where b.billType = 'pjtf' and b.isDeleted = 'false' and b.status = 'accepted'";
+		if(isset($q['c0']) && $q['c0'] != ""){
+			$sql .= ' and b.id like \'%'.$q['c0'].'%\'';
+		}
+		if(isset($q['payee']) && $q['payee'] != ""){
+			$sql .= ' and b.payee like \'%'.$q['payee'].'%\'';
 		}
 		return $this->parseData($sql,$q);
 	}
