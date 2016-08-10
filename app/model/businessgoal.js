75ef0c16cad001fe4a5452b7997da171f98ba776
alex123bob@126.com <alex123bob@126.com>
2016-08-10 20:33:17 +0800

    partially finish target setting

--- app/model/businessgoal.js
+++ app/model/businessgoal.js
@@ -2,17 +2,18 @@ Ext.define('FamilyDecoration.model.BusinessGoal', {
 	extend: 'Ext.data.Model',
 	fields: [
 		'id',
-		{name: 'user', type: 'string'},
+		{name: 'user', type: 'string', mapping: 'u'},
+        {name: 'userRealName', type: 'string', mapping: 'n'},
         // 目标量
-        {name: 'c1', type: 'string'}, // 市场部：扫楼，设计部：定金率
-        {name: 'c2', type: 'string'}, // 市场部：电销，设计部：签单额
-        {name: 'c3', type: 'string'}, // 市场部：到店
-        {name: 'c4', type: 'string'}, // 市场部：定金
+        {name: 'c1'}, // 市场部：扫楼，设计部：定金率
+        {name: 'c2'}, // 市场部：电销，设计部：签单额
+        {name: 'c3'}, // 市场部：到店
+        {name: 'c4'}, // 市场部：定金
         // 汇总量
-        {name: 'ac1', type: 'string'}, // 市场部：扫楼，设计部：定金率
-        {name: 'ac2', type: 'string'}, // 市场部：电销，设计部：签单额
-        {name: 'ac3', type: 'string'}, // 市场部：到店
-        {name: 'ac4', type: 'string'}, // 市场部：定金
+        {name: 'a1'}, // 市场部：扫楼，设计部：定金率
+        {name: 'a2'}, // 市场部：电销，设计部：签单额
+        {name: 'a3'}, // 市场部：到店
+        {name: 'a4'}, // 市场部：定金
         {name: 'targetMonth', type: 'string'},
 		{name: 'createTime', type: 'string'},
         {name: 'isDeleted', type: 'string'},
