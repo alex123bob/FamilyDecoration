4c5f05e3cc85fc6344520b6b006876256583cb58
diegozhu <674417307@qq.com>
2016-08-11 14:42:25 +0800

    fix bug

--- libs/svc/ErrorLogSvc.class.php
+++ libs/svc/ErrorLogSvc.class.php
@@ -2,6 +2,13 @@
 
 Class ErrorLogSvc extends BaseSvc{
 
+
+	public function delete($q){
+		global $mysql;
+		$mysql->DBExecute("delete from error_log where createTime = '".$q['createTime']."';");
+		return array('status'='successful');
+	}
+
 	public function testTransaction(){
 		global $mysql;
 		$this->add(array('@file'=>'1'));
