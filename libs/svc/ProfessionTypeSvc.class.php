5797062a6b10007fa7d9168a6eaafbd503a348f6
diegozhu <674417307@qq.com>
2016-08-16 11:53:38 +0800

    xx

--- libs/svc/ProfessionTypeSvc.class.php
+++ libs/svc/ProfessionTypeSvc.class.php
@@ -13,7 +13,7 @@ class ProfessionTypeSvc extends BaseSvc
 			$whereSql1 = parent::parseWhereSql('',$q,$params,'statement_bill');
 			$whereSql2 = parent::parseWhereSql('p.',$q,$params,'profession_type');
 			$orderBy = parent::parseOrderBySql($q);
-			$limit = parent::parseLimitSql($q);
+			$limit = "";//parent::parseLimitSql($q);
 			$sql = "select p.* ,IFNULL(b.h,0) as highLight,IFNULL(b2.h,0) as rdyck1 from profession_type p".
 					" left join ( ".
 						"select count(*) as h , professionType from statement_bill $whereSql0 group by professionType".
@@ -22,9 +22,9 @@ class ProfessionTypeSvc extends BaseSvc
 						"select count(*) as h , professionType from statement_bill $whereSql1 and status = 'rdyck1' group by professionType".
 					") b2 on b2.professionType = p.value $whereSql2 ";
 			$row = $mysql->DBGetAsMap($sql.$orderBy.$limit,$params);
-			$count = $mysql->DBGetAsOneArray("select count(*) from ( $sql ) as a",$params);
-			$count = $count[0];
-			return array('total'=>$count,'data'=>$row);
+			//$count = $mysql->DBGetAsOneArray("select count(*) from ( $sql ) as a",$params);
+			//$count = $count[0];
+			return array('total'=>count($row),'data'=>$row);
 		}else{
 			return parent::get($q);
 		}
