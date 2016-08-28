284b23107966c6d29109f86b680411adec658a3a
diegozhu <zhuhaiyang55@gmail.com>
2016-08-28 11:35:24 +0800

    supplier

---
+++ libs/svc/SupplierMaterialSvc.class.php
@@ -0,0 +1,12 @@
+<?php
+class SupplierMaterialSvc.class extends BaseSvc
+{
+	public function add($q){
+		notNullCheck($q,'@name','材料名不能为空!');
+		notNullCheck($q,'@professionType','工种不能为空!');
+		$q['@id'] = $this->getUUID();
+		return parent::add($q);
+	}
+}
+
+?>
