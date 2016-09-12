70b0b0818ecbed82262e54bb9ceda17df34c9b5b
alex123bob@126.com <alex123bob@126.com>
2016-09-12 16:49:47 +0800

    add logic operation in projectfinancemanagement module

--- app/view/projectfinancemanagement/index.js
+++ app/view/projectfinancemanagement/index.js
@@ -2,7 +2,8 @@ Ext.define('FamilyDecoration.view.projectfinancemanagement.Index', {
     extend: 'Ext.container.Container',
     alias: 'widget.projectfinancemanagement-index',
     requires: [
-        'FamilyDecoration.view.progress.ProjectListByCaptain'
+        'FamilyDecoration.view.progress.ProjectListByCaptain',
+        'FamilyDecoration.store.WorkCategory'
     ],
     // autoScroll: true,
     layout: 'hbox',
@@ -14,7 +15,19 @@ Ext.define('FamilyDecoration.view.projectfinancemanagement.Index', {
         var me = this;
 
         function _getRes() {
+            var projectList = me.getComponent('treepanel-projectList'),
+                projectListSelModel = projectList.getSelectionModel(),
+                projectSt = projectList.getStore(),
+                project = projectListSelModel.getSelection()[0],
+                projectCategory = me.getComponent('gridpanel-projectCategory');
 
+            return {
+                projectList: projectList,
+                projectListSelModel: projectListSelModel,
+                projectSt: projectSt,
+                project: project,
+                projectCategory: projectCategory
+            };
         };
 
         me.items = [
@@ -22,9 +35,75 @@ Ext.define('FamilyDecoration.view.projectfinancemanagement.Index', {
                 xtype: 'progress-projectlistbycaptain',
                 searchFilter: true,
                 title: '工程项目',
-                itemId: 'projectlist',
+                itemId: 'treepanel-projectList',
                 loadAll: false,
-                flex: 1
+                flex: 1,
+                style: {
+                    borderRightStyle: 'solid',
+                    borderRightWidth: '1px'
+                },
+                bbar: [
+                    {
+                        text: '汇总',
+                        icon: 'resources/img/sum.png',
+                        handler: function () {
+                            var resObj = _getRes();
+                            resObj.projectListSelModel.deselectAll();
+                            resObj.projectCategory.hide();
+                        }
+                    }
+                ],
+                listeners: {
+                    selectionchange: function (selModel, sels, opts) {
+                        var resObj = _getRes();
+                        if (resObj.project) {
+                            if (resObj.project.get('projectName')) {
+                                resObj.projectCategory.isHidden() && resObj.projectCategory.show();
+                            }
+                            else {
+
+                            }
+                        }
+                        else {
+                            
+                        }
+                    }
+                }
+            },
+            {
+                xtype: 'gridpanel',
+                width: 100,
+                title: '项目',
+                hideHeaders: true,
+                itemId: 'gridpanel-projectCategory',
+                columns: {
+                    defaults: {
+                        flex: 1,
+                        align: 'center'
+                    },
+                    items: [
+                        {
+                            text: '项目',
+                            dataIndex: 'name'
+                        }
+                    ]
+                },
+                style: {
+                    borderRightStyle: 'solid',
+                    borderRightWidth: '1px'
+                },
+                store: FamilyDecoration.store.WorkCategory,
+                listeners: {
+                    afterrender: function (grid, opts) {
+                        var st = grid.getStore();
+                        st.add(
+                            {
+                                name: '合计',
+                                value: '0000'
+                            }
+                        );
+                    }
+                }
             },
             {
                 xtype: 'container',
