ae5c30ee1ebac9ddb41c4142a8359a4b0aea04a0
diegozhu <zhuhaiyang55@gmail.com>
2016-09-18 15:35:18 +0800

    material order pdf

---
+++ fpdf/material_order.php
@@ -0,0 +1,104 @@
+<?php
+global $CellWidth,$FirstCellWidth,$titleLeft,$GfontSize,$lineHeight,$UserBrowserClient,$budgetName; 
+if(strpos($_SERVER["HTTP_USER_AGENT"],"Safari") && !strpos($_SERVER["HTTP_USER_AGENT"],"Chrome") ){
+	$UserBrowserClient = 'safari';
+}else if(strpos($_SERVER["HTTP_USER_AGENT"],"Mac") && strpos($_SERVER["HTTP_USER_AGENT"],"Chrome")){
+	$UserBrowserClient = 'chrome_mac';
+}else{
+	$UserBrowserClient = 'default';
+}
+
+global $name,$phone,$times,$address,$totalFee,$captain,$finishPercentage,$cny,$billId; 
+
+$lineHeight 	= 6;
+//全局字体
+$GfontSize		= 10;
+$GfontStyle		= ''; // B bold,U:underline
+
+
+include_once "../libs/conn.php";
+include_once 'chinese.php';
+include_once 'pdf_chinese_material_order.php';
+
+$billSvc = BaseSvc::getSvc('SupplierOrder');
+$supplierSvc = BaseSvc::getSvc('Supplier');
+$billItemSvc = BaseSvc::getSvc('SupplierOrderItem');
+$projectSvc = BaseSvc::getSvc('Project');
+
+$bill = $billSvc->getWithSupplier($_REQUEST);
+if(count($bill['data']) == 0){
+	echo '<html><script type="text/javascript">document.write(decodeURIComponent("%E6%89%BE%E4%B8%8D%E5%88%B0%E5%AF%B9%E5%BA%94%E7%9A%84%E5%8D%95%E6%8D%AE!"));</script></html>';
+	die();
+}
+$bill = $bill['data'][0];
+$billId = $bill['id'];
+$supplier = $supplierSvc->get(array('id'=>$bill['supplierId']))['data'][0];
+
+//var_dump($bill);
+//var_dump($supplier);
+$name = str2GBK($supplier['name']);
+$phone = str2GBK($bill['phoneNumber']);
+$projects = $projectSvc->get(array('projectId'=>$bill['projectId']));
+$captain = str2GBK($projects['data'][0]['captain']);
+$times = $bill['payedTimes'];
+$address = str2GBK($bill['projectName']);
+$totalFee = $bill['totalFee'];
+$finishPercentage = str2GBK($bill['projectProgress']);
+$cny = str2GBK(cny($totalFee));
+
+$billItems = $billItemSvc->get(array('billId'=>$_REQUEST['id']));
+$billItems = $billItems['data'];
+$action = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "view";
+$pdf=new PDF('P','mm', 'A4'); //创建新的FPDF对象 
+$pdf->AddGBFont(); //设置中文字体 
+$pdf->Open(); //开始创建PDF 
+$pdf->AddPage(); //增加一页 
+$pdf->SetFont("GB",$GfontStyle,$GfontSize); //设置字体样式 
+$pdf->AliasNbPages("__totalPage__");
+$titles = array('序号','项目','单位','数量','单价(元)','小计(元)');
+$widths = array(10,80,20,20,20,35);
+$aligns = array('C','C','C','C','C','C');
+$pdf->writeCellLine($widths,$titles,'LTBR','','C',6,10,$fontStyles = array());
+$totalBillCount = 0;
+foreach($billItems as $value) {
+	$data = array();
+	$data[0] = $value['referenceNumber'];
+	$data[1] = str2GBK($value['billItemName']);
+	$data[2] = str2GBK($value['unit']);
+	$data[3] = $value['amount'];
+	$data[4] = $value['unitPrice'];
+	$data[5] = $value['subtotal'];
+	$totalBillCount += $value['subtotal'];
+	$pdf->writeCellLine($widths,$data,'LTBR','',$aligns,6,10,$fontStyles = array());
+}
+global $lineHeight;
+$before = $lineHeight;
+$lineHeight = 12;
+$pdf->writeCellLine($widths,array('','合计','','','',$totalBillCount),'LTBR','','C',6,10,$fontStyles = array());
+$lineHeight = $before;
+
+$pdf->Cell(10,5,'','L','','L');
+$pdf->Cell(175,5,'','R','','L');
+$pdf->ln();
+$pdf->Cell(10,5,'','L','','L');
+$pdf->Cell(175,5,'','R','','L');
+$pdf->ln();
+
+$pdf->Cell(10,5,'','L','','L');
+$pdf->Cell(115,5,'大写金额: '.str2GBK(cny($totalBillCount)),'','','L');
+$pdf->Cell(60,5,'领款人(签字):','R','','L');
+$pdf->ln();
+
+$pdf->Cell(10,5,'','L','','L');
+$pdf->Cell(145,5,'','','','L');
+$pdf->Cell(30,5,'年     月      日','R','','L');
+$pdf->ln();
+$pdf->Cell(10,5,'','LB','','L');
+$pdf->Cell(175,5,'','RB','','L');
+
+
+$pdf->Ln();
+$pdf->Cell(11,21,"");
+$pdf->Ln();
+$pdf->Output($address.".pdf", $action == "view" ? "I" : "D" );
+?>  
---
+++ fpdf/pdf_chinese_material_order.php
@@ -0,0 +1,127 @@
+<?php
+
+class PDF extends PDF_Chinese{
+	
+	function Header(){ //设置页眉 
+	
+		global $name,$phone,$times,$address,$captain,$totalFee,$finishPercentage,$cny,$billId; 
+		
+		$this->SetFont('GB','',13); 
+		$this->SetLeftMargin(15);
+		$this->SetRightMargin(15);
+		//$this->SetTopMargin(15);
+		$this->SetAutoPageBreak(true,10);
+		//$this->SetTopMargin(5);
+		//$this->Image('../resources/img/logo.jpg',60,4,30,30); //增加一张图片，文件名为sight.jpg 
+		$this->Image('../resources/img/logo.jpg',50,4,22.5,22.5); //增加一张图片，文件名为sight.jpg 
+		$this->Text(80,15,'佳诚装饰');
+		$this->Text(80,22,'工程材料订购单 ');
+		$this->SetFont('GB','',8); 
+		$this->Text(158,30,"公司联-$billId");
+		$this->Ln(5); //换行
+		$this->SetFont('GB','',10); 
+		$this->Text(35,40,'供应商:');
+		$this->Text(47,40,$name);
+		$this->Text(35,48,"第$times 次领款,完成 ".$finishPercentage);
+		$this->Text(80,40,'项目经理:');
+		$this->Text(97,40,$captain);
+		$this->Text(130,40,'工程地址:');
+		$this->Text(148,40,$address);
+		$this->Text(80,48,'总金额:');
+		$this->Text(93,48,$totalFee.' (元)');
+		$this->Text(130,48,'大写:');
+		$this->Text(147,48,$cny);
+		$this->Text(35,56,'联系电话:');
+		$this->Text(51,56,$phone);
+
+		//$this->Line(10,50,280,50);
+		$this->Ln(45);
+	} 
+	function Footer(){ //设置页脚  
+		$this->SetY(-10); 
+		global $GfontSize;
+		$this->SetFont('GB','',$GfontSize); 
+		$this->Cell(120,10,'第'.$this->PageNo().'页  共__totalPage__页',0,0,'R'); 
+		//$this->Cell(220,10,date("Y-m-d"),0,0,'R'); 
+	}
+	// 输出一行表格
+	function writeCellLine($widths,$txts,$borders,$positions,$aligns,$times=6,$fontSizes=10,$fontStyles = array()){
+		global $lineHeight;
+		$c = 0;
+		$thisLineHeight = $lineHeight;
+		//如果表格内容过大，则需要自动换行，因此，有时候不仅输出一行，
+		//而是输出好几行，先遍历所有表格，判断需要输出多少行
+		for( ; $c < $times ; $c ++){
+			$thisLineFontSize = $fontSizes;
+			$thisLineFontStyle = '';
+			if($fontSizes != null && is_array($fontSizes) && isset($fontSizes[$c])){
+				$thisLineFontSize = $fontSizes[$c];
+			}
+			if($fontStyles != null && is_array($fontStyles) && isset($fontStyles[$c])){
+				$thisLineFontStyle = $fontStyles[$c];
+			}
+			$this->SetFont('GB',$thisLineFontStyle,$thisLineFontSize);
+			$txt = is_array($txts) ? $txts[$c] : $txts;
+			$txt = ($txt === null || $txt === "NULL")? "" : $txt;
+			$w = is_array($widths) ? $widths[$c] : $widths;
+			// 计算需要几行 .
+			$linesNeed = $this->GetStringShowLines($txt,$w);
+			if($linesNeed == 1){
+				// do nothing , use default;
+			}else{
+				// multi lines.
+				$tmpLineHeight = ( $c == 6 ? 4 : 5);
+				if(!$this->ContainsChinese($txt)){
+					//没有中文的话，可以行高再减少1
+					$tmpLineHeight--;
+				};
+				$tmp = $linesNeed * $tmpLineHeight;
+				$thisLineHeight = $thisLineHeight > $tmp ?  $thisLineHeight : $tmp;
+			}
+		}
+		
+		$c = 0;
+		$lastCellHeight;
+		//遍历所有表格，输出
+		for( ; $c < $times ; $c ++){
+			$this->cMargin = $c == 1 || $c == 6 ? 1 : 0;
+			$thisLineFontSize = $fontSizes;
+			if($fontSizes != null && is_array($fontSizes) && isset($fontSizes[$c])){
+				$thisLineFontSize = $fontSizes[$c];
+			}
+			$this->SetFont('GB',$thisLineFontStyle,$thisLineFontSize);
+			$txt = is_array($txts) ? $txts[$c] : $txts;
+			$txt = ($txt === null || $txt === "NULL")? "" : $txt;
+			$w = is_array($widths) ? $widths[$c] : $widths;
+			$border = is_array($borders) ? $borders[$c] : $borders;
+			$align = is_array($aligns) ? $aligns[$c] : $aligns;
+			$linesNeed = $this->GetStringShowLines($txt,$w);
+			
+			if($linesNeed > 1){
+				$x = $this->getx();
+				$y = $this->gety();
+				if($c == 1 || $c == 6){
+					//第二列和最后一列靠左对齐。其他居中
+					$align = 'L';
+				}
+				$tmpLineHeight = ( $c == 6 ? 4 : 5);
+				if(!$this->ContainsChinese($txt)){
+					//没有中文的话，可以行高再减少1
+					$tmpLineHeight--;
+				};
+				$tmp = $linesNeed * $tmpLineHeight;
+				$lastCellHeight = $this->MultiCell($w,$tmpLineHeight,$txt,$border,$align,false,$thisLineHeight);
+				$this->setxy($x+$w,$y);
+			}else{
+				$this->Cell($w,$thisLineHeight,$txt,$border,'R',$align);
+				$lastCellHeight = $thisLineHeight;
+			}
+		}
+
+		$x = $this->getx();
+		$y = $this->gety();
+		$this->setxy($x,$y+$thisLineHeight-$lastCellHeight);
+		$this->Ln();
+	}
+}
+?>
