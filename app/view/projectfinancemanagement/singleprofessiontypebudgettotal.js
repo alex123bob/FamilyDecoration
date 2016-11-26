b0a67543bf6273f66527ef929a0cceb815cb7da3
alex123bob@126.com <alex123bob@126.com>
2016-11-27 01:02:56 +0800

    change interface

--- app/view/projectfinancemanagement/singleprofessiontypebudgettotal.js
+++ app/view/projectfinancemanagement/singleprofessiontypebudgettotal.js
@@ -32,7 +32,7 @@ Ext.define('FamilyDecoration.view.projectfinancemanagement.SingleProfessionTypeB
         });
 
         Ext.apply(costDiffProxy.extraParams, {
-            action: 'Project.get',
+            action: 'Project.financeReport',
             projectId: me.projectId
         });
 
