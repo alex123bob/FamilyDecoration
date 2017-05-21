7f46e1ee5d2814d4352812d40eaeceb6d6365765
alex123bob@126.com <alex123bob@126.com>
2017-05-21 22:42:57 +0800

    fix pdf deadlock issue, by diego.

--- fpdf/pdf_chinese_planlabor.php
+++ fpdf/pdf_chinese_planlabor.php
@@ -8,7 +8,10 @@ class PDF extends PDF_Chinese{
 		
 		$this->SetLeftMargin(10);
 		$this->SetRightMargin(10);
-		$this->SetAutoPageBreak(true,5);
+		//$this->SetTopMargin(15);
+		$this->SetAutoPageBreak(true,10);
+		//$this->SetTopMargin(5);
+		//$this->Image('../resources/img/logo.jpg',60,4,30,30); //增加一张图片，文件名为sight.jpg 
 		$this->SetFont('GB','',25); 
 		$imgWidth = 17;
 		$title = '  佳诚装饰    '.$professionTypes[$_REQUEST['professionType']].'用工计划时间表';
@@ -20,10 +23,13 @@ class PDF extends PDF_Chinese{
 		$this->SetFont('','',11); 
 		$leftOffset = ($this->w - $this->getstringwidth($firstLine))/2;
 		$this->Text($leftOffset,25,$firstLine);
+		
+
+		//$this->Line(10,50,280,50);
 		$this->Ln(20);
 	} 
 	function Footer(){ //设置页脚  
-		//$this->SetY(-10); 
+		$this->SetY(-10); 
 		global $GfontSize;
 		$this->SetFont('GB','',$GfontSize); 
 		$this->Cell(0,10,'第'.$this->PageNo().'页  共__totalPage__页',0,0,'C'); 
