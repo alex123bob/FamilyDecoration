0168f0517ebdaf7bbe97fe215766f03df543258a
alex123bob@126.com <alex123bob@126.com>
2017-07-17 11:17:13 +0800

    add custom request header into allowed-header

--- libs/conn.php
+++ libs/conn.php
@@ -3,7 +3,7 @@
 	header("message-queue: 1");
 	header( 'Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);
 	header( 'Access-Control-Allow-Credentials:true');
-	header( 'Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept');
+	header( 'Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, app');
 
 	$isLocal = $_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1';
     if(!$isLocal && $_SERVER['HTTPS'] != 'on') {
