3b47c2c2b36b56b031ba9ba4aa8e8303b494d60b
alex123bob@126.com <alex123bob@126.com>
2017-07-19 10:40:21 +0800

    change createTime default value

--- sql/version-9.4.sql
+++ sql/version-9.4.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `user` ADD `supplierId` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '供應商id' AFTER `projectId`;
+ALTER TABLE `user` CHANGE `createTime` `createTime` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间';
 
 insert into `versions`(`id`) values ('version-9.4');
