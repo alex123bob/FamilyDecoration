2cebe6ac00aafab041d3d6ec24a266fe624c53ce
diegozhu <diego@diegozhu.net>
2017-07-23 01:20:32 +0800

    xxx

---
+++ sql/version-9.8.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `supplier_material_audit` add `approved` VARCHAR(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL default 'false' COMMENT '是否通过审核';
+ALTER TABLE `supplier_material_audit` add `approver` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL default null COMMENT '是否通过审核';
+
+insert into `versions`(`id`) values('version-9.7');
