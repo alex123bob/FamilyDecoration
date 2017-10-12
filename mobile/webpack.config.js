2541d67f3e90b30c4a9dc9b03a522633e3ddb8a3
alex123bob <alex123bob@126.com>
2017-10-12 18:18:49 +0800

    add webpack bundle analyzer for project.

--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -1,9 +1,19 @@
 const webpack = require('webpack');
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (webpackConfig, env) => {
     // webpackConfig.output.chunkFilename = '[name].[chunkhash].js';
 
-    webpackConfig.output.filename = '[name].bundle.[hash].js';
+    // webpackConfig.output.filename = '[name].bundle.[hash].js';
+
+    webpackConfig.plugins = webpackConfig.plugins.concat(
+        [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static'
+            })
+        ]
+    );
 
     return webpackConfig;
 }
