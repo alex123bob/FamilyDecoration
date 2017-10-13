24b0c155e741b210af14e9ad7a0898a1278a2d54
alex123bob <alex123bob@126.com>
2017-10-13 12:33:47 +0800

    reconfigure entry, output and common modules extraction. utilize webpack + roadhog to make website loading process much better.

--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -1,17 +1,44 @@
 const webpack = require('webpack');
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (webpackConfig, env) => {
-    // webpackConfig.output.chunkFilename = '[name].[chunkhash].js';
+    webpackConfig.entry = {
+        app: './src/index.js',
+        vendor: ['lodash', 'dva', 'react', 'react-redux', 'redux']
+    };
 
-    // webpackConfig.output.filename = '[name].bundle.[hash].js';
+    webpackConfig.output = {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+        publicPath: env === 'production' ? './' : '/' // publicPath could be configured to any other address especially for CDN distribution in the future if needed.
+    };
 
     webpackConfig.plugins = webpackConfig.plugins.concat(
         [
-            new BundleAnalyzerPlugin({
-                analyzerMode: 'static'
-            })
+            new HtmlWebpackPlugin(
+                {
+                    template: 'src/index.ejs',
+                    minify: env === 'production' ? {
+                        collapseWhitespace: true
+                    } : false,
+                    hash: false // this determines wheter to append hash to the end of each linked resources, start with a question mark.
+                }
+            ),
+
+            new webpack.optimize.CommonsChunkPlugin(
+                {
+                    names: ['vendor'],
+                    filename: 'vendor.js',
+                    minChunks: Infinity
+                }
+            ),
+
+            // this is just for generating bundle report. for future analysis.
+            // new BundleAnalyzerPlugin({
+            //     analyzerMode: 'static'
+            // })
         ]
     );
 
