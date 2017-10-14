6d9b81f1f239c37f79435d63a6a38be3d5b40f7a
alex123bob@126.com <alex123bob@126.com>
2017-10-14 17:14:04 +0800

    add sass-loader, reconfigure webpack config

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import dva from 'dva';
-import './index.css';
+import './index.scss';
 
 // 1. Initialize
 const app = dva();
--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (webpackConfig, env) => {
   webpackConfig.entry = {
@@ -41,6 +42,28 @@ module.exports = (webpackConfig, env) => {
       // })
     ],
   );
+  webpackConfig.module.rules = webpackConfig.module.rules.concat(
+    [
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  );
+
+  // console.log(webpackConfig.module.rules);
 
   return webpackConfig;
 };
