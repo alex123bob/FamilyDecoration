cd24681e5aefd1754c5a6c16bd3df63b15161790
alex123bob@126.com <alex123bob@126.com>
2017-10-14 09:58:11 +0800

    code format including permission and webpack config file configuration.

--- mobile/webpack.config.js
+++ mobile/webpack.config.js
@@ -4,43 +4,43 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (webpackConfig, env) => {
-    webpackConfig.entry = {
-        app: './src/index.js',
-        vendor: ['lodash', 'dva', 'react', 'react-redux', 'redux']
-    };
+  webpackConfig.entry = {
+    app: './src/index.js',
+    vendor: ['lodash', 'dva', 'react', 'react-redux', 'redux'],
+  };
 
-    webpackConfig.output = {
-        filename: '[name].bundle.js',
-        path: __dirname + '/dist',
-        publicPath: env === 'production' ? './' : '/' // publicPath could be configured to any other address especially for CDN distribution in the future if needed.
-    };
+  webpackConfig.output = {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: env === 'production' ? './' : '/', // publicPath could be configured to any other address especially for CDN distribution in the future if needed.
+  };
 
-    webpackConfig.plugins = webpackConfig.plugins.concat(
-        [
-            new HtmlWebpackPlugin(
-                {
-                    template: 'src/index.ejs',
-                    minify: env === 'production' ? {
-                        collapseWhitespace: true
-                    } : false,
-                    hash: false // this determines wheter to append hash to the end of each linked resources, start with a question mark.
-                }
-            ),
+  webpackConfig.plugins = webpackConfig.plugins.concat(
+    [
+      new HtmlWebpackPlugin(
+        {
+          template: 'src/index.ejs',
+          minify: env === 'production' ? {
+            collapseWhitespace: true,
+          } : false,
+          hash: false, // this determines wheter to append hash to the end of each linked resources, start with a question mark.
+        },
+      ),
 
-            new webpack.optimize.CommonsChunkPlugin(
-                {
-                    names: ['vendor'],
-                    filename: 'vendor.js',
-                    minChunks: Infinity
-                }
-            ),
+      new webpack.optimize.CommonsChunkPlugin(
+        {
+          names: ['vendor'],
+          filename: 'vendor.js',
+          minChunks: Infinity,
+        },
+      ),
 
-            // this is just for generating bundle report. for future analysis.
-            // new BundleAnalyzerPlugin({
-            //     analyzerMode: 'static'
-            // })
-        ]
-    );
+      // this is just for generating bundle report. for future analysis.
+      // new BundleAnalyzerPlugin({
+      //     analyzerMode: 'static'
+      // })
+    ],
+  );
 
-    return webpackConfig;
-}
+  return webpackConfig;
+};
