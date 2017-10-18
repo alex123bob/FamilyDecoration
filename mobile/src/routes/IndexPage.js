1a39c6765f617625ca1ef1ef480319e182dd9237
alex123bob@126.com <alex123bob@126.com>
2017-10-18 22:44:57 +0800

    add tab layout for project.

--- mobile/src/routes/IndexPage.js
+++ mobile/src/routes/IndexPage.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.less';
-import { Button } from 'antd-mobile';
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-      <Button type="primary">default</Button>
-    </div>
-  );
-}
+import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
 
-IndexPage.propTypes = {
-};
+const tabs = [
+  { title: <Badge text={'3'}>员工工资</Badge> },
+  { title: <Badge text={'今日(20)'}>待定板块</Badge> },
+  { title: <Badge dot>我的</Badge> },
+];
 
-export default connect()(IndexPage);
+const TabCt = () => (
+  <div>
+    <Tabs tabs={tabs}
+      initialPage={1}
+      onChange={(tab, index) => { console.log('onChange', index, tab); }}
+      onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: document.documentElement.clientHeight, backgroundColor: '#fff' }}>
+        员工工资
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: document.documentElement.clientHeight, backgroundColor: '#fff' }}>
+        待开发
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: document.documentElement.clientHeight, backgroundColor: '#fff' }}>
+        我的
+      </div>
+    </Tabs>
+    <WhiteSpace />
+    <WhiteSpace />
+  </div>
+);
+
+export default connect()(TabCt);
