f4d494abeb8fe8ba26ec24a55370b061c007c380
alex123bob <alex123bob@126.com>
2018-02-22 12:11:52 +0800

    use highchart. build pie chart and donut based on regions in total and data grouped by level.

--- chart.php
+++ chart.php
@@ -4,97 +4,164 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        body {
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            width: 960px;
-            height: 500px;
-            position: relative;
-        }
-        path.slice{
-            stroke-width:2px;
-        }
-        polyline{
-            opacity: .3;
-            stroke: black;
-            stroke-width: 2px;
-            fill: none;
-        } 
-        svg text.percent{
-            fill:white;
-            text-anchor:middle;
-            font-size:12px;
-        }
-    </style>
     <title>FamilyDecoration Data Analyses</title>
-    <script src="chart/d3.v3.min.js"></script>
-    <script src="chart/Donut3D.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
 </head>
 <body>
+    <div id="regionCountPie" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+    <div id="communityCountByLevel" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
     <script>
-        var salesData=[
-            {label:"Basic", color:"#3366CC"},
-            {label:"Plus", color:"#DC3912"},
-            {label:"Lite", color:"#FF9900"},
-            {label:"Elite", color:"#109618"},
-            {label:"Delux", color:"#990099"}
-        ];
+        let regionMap = {}, regions = [];
+        // Radialize the colors
+        Highcharts.setOptions({
+            colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            })
+        });
 
-        var regionMap = {},
-            regions = [];
-
-        function getColor (){
-            var arr = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'],
-                color = '#',
-                counter = 0;
-            while (counter < 6) {
-                color += arr[Math.floor(15*Math.random())];
-                counter++;
+        function buildDonutByCommunityLevel (regionId) {
+            if (Object.keys(regionMap).length === 0 && regionMap.constructor === Object) {
+                return false;
             }
-            return color;
-        }
-
-        var svg = d3.select("body").append("svg").attr("width",2000).attr("height",1000);
-
-        svg.append("g").attr("id","salesDonut");
-        svg.append("g").attr("id","quotesDonut");
-
-        // Donut3D.draw("salesDonut", randomData(), 150, 150, 130, 100, 30, 0.4);
-        // Donut3D.draw("quotesDonut", randomData(), 450, 150, 130, 100, 30, 0);
-            
-        function changeData(){
-            // Donut3D.transition("salesDonut", randomData(), 130, 100, 30, 0.4);
-            // Donut3D.transition("quotesDonut", randomData(), 130, 100, 30, 0);
-        }
-
-        function randomData(){
-            return salesData.map(function(d){ 
-                return {label:d.label, value:1000*Math.random(), color:d.color};});
-        }
-
-        fetch('./libs/business.php?debug=true&action=getBusiness').then(res => res.json())
-        .then(arr => {
-            arr.forEach((obj, index) => {
-                if (regionMap[obj.regionId]) {
-                    regionMap[obj.regionId].value++;
+            let communities = regionMap[regionId].communities,
+                map = {},
+                arr = [];
+            communities.forEach((obj, index, self) => {
+                if (!obj.level) {obj.level = 'others'};
+                if (map[obj.level]) {
+                    map[obj.level].count++;
                 }
                 else {
-                    regionMap[obj.regionId] = {
-                        value: 1,
-                        label: obj.name,
-                        color: getColor()
-                    }
+                    map[obj.level] = {
+                        level: obj.level,
+                        count: 1
+                    };
                 }
-            })
-            for (const pro in regionMap) {
-                if (regionMap.hasOwnProperty(pro)) {
-                    const obj = regionMap[pro];
-                    regions.push(obj);
+            });
+            for (const key in map) {
+                if (map.hasOwnProperty(key)) {
+                    const el = map[key];
+                    arr.push([el.level, el.count]);
                 }
             }
-            Donut3D.draw("salesDonut", regions, 1000, 400, 700, 300, 30, 0.4);
+            Highcharts.chart('communityCountByLevel', {
+                chart: {
+                    type: 'pie',
+                    options3d: {
+                        enabled: true,
+                        alpha: 45
+                    }
+                },
+                title: {
+                    text: 'Community Aggregation By Level'
+                },
+                subtitle: {
+                    text: '3D donut in Highcharts'
+                },
+                plotOptions: {
+                    pie: {
+                        innerSize: 100,
+                        depth: 45
+                    }
+                },
+                series: [{
+                    name: 'Level Amount',
+                    data: arr
+                }]
+            });
+        }
+
+        function buildPieByRegionalCommunities (regions){
+            // Build the chart
+            Highcharts.chart('regionCountPie', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Regional Communities Counting'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y}</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        point: {
+                            events: {
+                                click: function () {
+                                    buildDonutByCommunityLevel(this.regionId);
+                                }
+                            }
+                        },
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            },
+                            connectorColor: 'silver'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'Community Counts',
+                    data: regions
+                }]
+            });
+        }
+
+        const promise = new Promise((resolve, reject) => {
+            fetch('./libs/business.php?debug=true&action=getBusiness').then(res => res.json())
+            .then(arr => {
+                if (arr.length > 0) {
+                    arr.forEach((obj, index) => {
+                        if (regionMap[obj.regionId]) {
+                            regionMap[obj.regionId].y++;
+                            regionMap[obj.regionId].communities.push(obj);
+                        }
+                        else {
+                            regionMap[obj.regionId] = {
+                                regionId: obj.regionId,
+                                y: 1,
+                                name: obj.name,
+                                communities: [obj]
+                            }
+                        }
+                    })
+                    for (const pro in regionMap) {
+                        if (regionMap.hasOwnProperty(pro)) {
+                            const obj = regionMap[pro];
+                            regions.push(obj);
+                        }
+                    }
+                    resolve(regions);
+                }
+                else {
+                    reject(arr);
+                }
+            });  
+        });
+        promise.then((regions) => {
+            buildPieByRegionalCommunities(regions);
+        }, (err) => {
+            console.log(err);
         })
     </script>
-    <input type="button" value="change data" onclick="changeData()" />
 </body>
 </html>
