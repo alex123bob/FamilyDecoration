73dc7357d8b6f4e9567e0ae1a26220ff940ecd30
diegozhu <diego@diegozhu.net>
2021-03-01 14:36:45 +0800

    Update CostListItemSvc.class.php

--- libs/svc/CostListItemSvc.class.php
+++ libs/svc/CostListItemSvc.class.php
@@ -8,4 +8,15 @@ class CostListItemSvc extends BaseSvc
 		$q['@id'] = $this->getUUID();
 		return parent::add($q);
 	}
+	
+	public function update($q){
+		notNullCheck($q,'id','id不能为空!');
+		notNullCheck($q,'version','version不能为空!');
+		notNullCheck($q,'@name','名称不能为空!');
+		notNullCheck($q,'@unit','单位不能为空!');
+		notNullCheck($q,'@professionType','工种不能为空!');
+		$q['@version'] = intval($q['version']) + 1;
+		$q['@id'] = intval($q['id']) + 1;
+		return parent::add($q);
+	}
 }
