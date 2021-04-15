7dcac48cf1722fd7a8c2286bb41e07adfa7391a0
DESKTOP-KDM722F <>
2021-04-15 23:24:41 +0800

    fix performance issue.

--- libs/svc/BidProjectSvc.class.php
+++ libs/svc/BidProjectSvc.class.php
@@ -8,6 +8,10 @@ class BidProjectSvc extends BaseSvc
     }
 
 	public function get($q){
+		// $this->appendSelect = ', b.*';
+		// $this->appendJoin = ' left join StatementBill b on bid_project.id = b.refId';
+		// return $this->get($q);
+
 		$res = parent::get($q);
 		foreach ($res['data'] as $key => &$obj) {
 			$bill = parent::getSvc('StatementBill')->get(array('refId'=>$obj['id'])); // involve corresponding bill item, need display bill status in front-end grid.
