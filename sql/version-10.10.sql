015270565d7015069248203d1ab735f9b6c1d591
DESKTOP-KDM722F <>
2021-04-16 17:37:20 +0800

    update sql

--- sql/version-10.10.sql
+++ sql/version-10.10.sql
@@ -1,3 +1,5 @@
 ALTER TABLE statement_bill modify billType varchar(20)  comment 'ppd:预付款,reg:普通账单,qgd:质量保证金,mtf:材料付款,rbm:报销,fdf:财务部门费用,wlf:福利,tax:税,bidbond:投标保证金,bidbondBk投标保证金（退回）,pmbond履约保证金,pmbondBk履约保证金（退回）';
+ALTER TABLE statement_bill modify payer varchar(20) comment '财务/出纳 (入账时是收款人，出账时是付款人)';
+ALTER TABLE statement_bill modify payee varchar(20) comment '财务/出纳对接方 (入账付款人，出账收款人)';
 
 insert into `versions`(`id`) values ('version-10.10');
