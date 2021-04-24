61a55bdfcd98e92f42352af604df7ee023b1e3e1
DESKTOP-KDM722F <>
2021-04-24 15:16:58 +0800

    add getSupplierTypes

--- libs/svc/SupplierSvc.class.php
+++ libs/svc/SupplierSvc.class.php
@@ -1,20 +1,26 @@
 <?php
 class SupplierSvc extends BaseSvc
 {
-	public function add($q){
-		notNullCheck($q,'@name','供应商名不能为空!');
+	public function add($q)
+	{
+		notNullCheck($q, '@name', '供应商名不能为空!');
 		$q['@id'] = $this->getUUID();
 		return parent::add($q);
 	}
 
-	public function getSupplierTypes() {
+	public function getSupplierTypes()
+	{
 		return array(
-			array('value'=> 'material', 'name'=> '材料供应商'),
-			array('value'=> 'device', 'name'=> '设备供应商')
+			'status' => 'successful',
+			'data' => array(
+				array('value' => 'material', 'name' => '材料供应商'),
+				array('value' => 'device', 'name' => '设备供应商')
+			)
 		);
 	}
 
-	public function get($q) {
+	public function get($q)
+	{
 		global $mysql;
 		$res = parent::get($q);
 		if (isset($_SESSION["supplierId"])) {
@@ -29,5 +35,3 @@ class SupplierSvc extends BaseSvc
 		return $res;
 	}
 }
-
-?>
