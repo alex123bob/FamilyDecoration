8053b37a2cdf59857839a95e3d1a361db10b4e80
alex123bob <alex123bob@126.com>
2023-05-04 10:46:06 +0800

    Archive reimbursement bill that's been paid.

---
+++ sql/version-2023-05-04.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `statement_bill` CHANGE `status` `status` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '\'new\', \'rdyck\', \'chk\',\'rbk\', \'paid\', \'archive\'';
+UPDATE `statement_bill` SET `status` = 'archive' WHERE `billType` = 'rbm' and `isDeleted` = 'false' and `status` = 'paid'
+insert into `versions`(`id`) values ('version-2023-05-04');
